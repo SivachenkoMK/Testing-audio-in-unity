47f129fc2aaf7d046590ec4717ead47ed91c606f
Syvachenko Misha <misha.sivachenko@gmail.com>
2020-03-17 19:57:11 +0200

    Initial commit

---
+++ AudioHW/Assets/ReverseSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReverseSound : MonoBehaviour
+{
+	public AudioSource source;
+	public Slider slider;
+
+	public void Reverse()
+	{
+		source.volume = 1 - source.volume;
+	}
+
+	public void ChangeValue()
+	{
+		slider.value = 1 - slider.value;
+	}
+}
